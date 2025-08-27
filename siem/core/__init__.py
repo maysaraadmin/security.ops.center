19c4c0a671250a11c4a91596c05b431871134cfd
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-08-27 23:08:57 +0400

    io

---
+++ siem/core/__init__.py
@@ -0,0 +1,10 @@
+"""
+SIEM Core Package
+
+This package contains the core functionality of the SIEM system.
+"""
+
+from .siem import SIEM
+from .component import Component
+
+__all__ = ['SIEM', 'Component']
